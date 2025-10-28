9c662b4cc64381fdfdb3a8b1c185964f3cb1ca07
zayahan <zayahanshah.18@gmail.com>
2025-10-28 19:03:04 +0500

    add refresh, logout api in api url file

--- lib/src/core/constants/api_url.dart
+++ lib/src/core/constants/api_url.dart
@@ -2,6 +2,8 @@ class ApiUrl {
   static const String baseUrl = 'http://192.168.100.154:5030';
   static const String signupUrl = '$baseUrl/api/Auth/signup';
   static const String loginUrl = '$baseUrl/api/Auth/login';
+  static const String refreshTokenUrl = '$baseUrl/api/Auth/refresh';
+  static const String logoutUrl = '$baseUrl/api/Auth/logout';
   static const String getBillsByConsumerNoUrl = '$baseUrl/api/ConsumerNumber/consumer/';
   static const String registerConsumerNoUrl = '$baseUrl/api/ConsumerNumber/register';
   static const String getConsumerNoOfUser = '$baseUrl/api/ConsumerNumber/user/';
